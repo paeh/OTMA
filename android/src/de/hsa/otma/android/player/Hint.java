4441e6cd52405ab0625ae44eef73d85a14f7573e
Johannes Leimer <johannes.leimer@hs-augsburg.de>
2012-06-12 17:18:12 +0200

    #65 added accessor methods

--- android/src/de/hsa/otma/android/player/Hint.java
+++ android/src/de/hsa/otma/android/player/Hint.java
@@ -1,13 +1,38 @@
 package de.hsa.otma.android.player;
 
 public class Hint {
-    
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getText() {
+        return text;
+    }
+
     private String title;
-    
+
     private String text;
 
     public Hint(String title, String text) {
         this.title = title;
         this.text = text;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Hint hint = (Hint) o;
+
+        if (title != null ? !title.equals(hint.title) : hint.title != null) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        return title != null ? title.hashCode() : 0;
     }
 }
--- android/src/de/hsa/otma/android/player/PlayerService.java
+++ android/src/de/hsa/otma/android/player/PlayerService.java
@@ -4,13 +4,14 @@ import android.util.Log;
 import de.hsa.otma.android.map.*;
 
 public class PlayerService {
+    private static final String TAG = PlayerService.class.getSimpleName();
     public static final PlayerService INSTANCE = new PlayerService();
 
-    private Player humanPlayer;
+    private HumanPlayer humanPlayer;
     private static final Board BOARD = Board.INSTANCE;
 
     private PlayerService() {
-        humanPlayer = new Player(new Coordinate(1, 1), "human");
+        humanPlayer = new HumanPlayer(new Coordinate(1, 1), "human");
     }
 
     public BoardElement move(Direction direction) {
@@ -24,14 +25,30 @@ public class PlayerService {
             newMapItem = currentMapItem.getElementFor(direction);
         }
 
-        Log.e("TAG", "current position = " + currentMapItem);
+        Log.e(TAG, "current position = " + currentMapItem);
 
         if (newMapItem == null) return null;
 
-        Log.e("TAG", "moving to " + newMapItem);
+        Log.e(TAG, "moving to " + newMapItem);
         humanPlayer.moveTo(newMapItem.getCoordinate());
 
         return newMapItem;
+    }
+
+    public void foundHint(Hint hint) {
+        humanPlayer.found(hint);
+        Log.i(TAG, "found hint '" + hint.getTitle()  +"'");
+    }
+
+    public void foundNPC(NPCPlayer player) {
+        humanPlayer.found(player);
+        Log.i(TAG, "found npc '" + player.getName() +"'");
+    }
+
+    public boolean hasChallengeCompleted() {
+
+        // TODO code to predict challenge completion
+        return false;
     }
 
     public BoardElement getCurrentMapItem() {
