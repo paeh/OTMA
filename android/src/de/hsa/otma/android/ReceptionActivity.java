ae931eb32c8f578856d5219f75f70f9fe537931c
Florian Wiedenmann <theannz@web.de>
2012-06-26 16:40:00 +0200

    #69 Create reception activity - add listener to enter button to start MapViewActivity

--- android/src/de/hsa/otma/android/ReceptionActivity.java
+++ android/src/de/hsa/otma/android/ReceptionActivity.java
@@ -1,6 +1,7 @@
 package de.hsa.otma.android;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -16,6 +17,7 @@ public class ReceptionActivity extends Activity{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         createLayout();
+        setUpEnterButton();
     }
 
     private void createLayout() {
@@ -44,4 +46,14 @@ public class ReceptionActivity extends Activity{
         askButton.setVisibility(View.INVISIBLE);
     }
 
+    private void setUpEnterButton(){
+        Button enterButton = (Button) findViewById(R.id.enterButton);
+        enterButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent mapViewIntent = new Intent(ReceptionActivity.this, MapViewActivity.class);
+                ReceptionActivity.this.startActivity(mapViewIntent);
+            }
+        });
+    }
 }
